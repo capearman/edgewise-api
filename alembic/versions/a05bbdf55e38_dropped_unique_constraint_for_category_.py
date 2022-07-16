7363955ed20a273c4e33a370d815b3e6ce66d80f
Caleb <caleb.a.pearman@gmail.com>
2022-07-16 10:37:15 -0400

    got rid of the unique constraint for the category column in the transactions table so that multiple transactions can be assigned to a category

---
+++ alembic/versions/a05bbdf55e38_dropped_unique_constraint_for_category_.py
@@ -0,0 +1,24 @@
+"""dropped unique constraint for category column in transactions table
+
+Revision ID: a05bbdf55e38
+Revises: fe293c75a6d3
+Create Date: 2022-07-16 10:24:13.647415
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a05bbdf55e38'
+down_revision = 'fe293c75a6d3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.drop_constraint('transactions_category_key', 'transactions', type_='unique')
+
+
+def downgrade() -> None:
+    op.create_unique_constraint('transactions_category_key', 'transactions', type_='unique')
